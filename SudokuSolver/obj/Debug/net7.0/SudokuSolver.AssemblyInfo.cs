70ad99dd8f73fea579e41cfebd25373e7fa34673
Zixi Tang <zixi.sam.tang@gmail.com>
2024-02-12 23:30:19 -0500

    Change to .Net 8

--- SudokuSolver/obj/Debug/net7.0/SudokuSolver.AssemblyInfo.cs
+++ SudokuSolver/obj/Debug/net7.0/SudokuSolver.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SudokuSolver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db05b11d9188a3c889dbe4fb8fe2191ff49afa35")]
 [assembly: System.Reflection.AssemblyProductAttribute("SudokuSolver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SudokuSolver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
